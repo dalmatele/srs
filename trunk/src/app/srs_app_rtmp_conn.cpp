c793923d0aeb9ce1de519de8e007bf04f277222b
ducla <ducla@uiza.io>
2019-08-02 15:18:03 +0700

    add log

--- trunk/src/app/srs_app_rtmp_conn.cpp
+++ trunk/src/app/srs_app_rtmp_conn.cpp
@@ -541,14 +541,12 @@ int SrsRtmpConn::stream_service_cycle()
             return ret;
         }
         case SrsRtmpConnFMLEPublish: {
-            srs_verbose("FMLE start to publish stream %s.", req->stream.c_str());
-            srs_trace("FMLE start to publish stream %s.", req->stream.c_str());
+            srs_verbose("FMLE start to publish stream %s.", req->stream.c_str());            
             if ((ret = rtmp->start_fmle_publish(res->stream_id)) != ERROR_SUCCESS) {
                 srs_error("start to publish stream failed. ret=%d", ret);
-                srs_trace("start to publish stream failed. ret=%d", ret);
                 return ret;
             }
-            
+            srs_trace("FMLE start to publish stream %s.", req->stream.c_str());
             return publishing(source);
         }
         case SrsRtmpConnHaivisionPublish: {
